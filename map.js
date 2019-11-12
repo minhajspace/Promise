e853d19debf33a56f90ae78489eba66494a337ff
minhajspace <minhaj.space@gmail.com>
2019-11-12 20:25:27 +0530

    first commit

---
+++ map.js
@@ -0,0 +1,28 @@
+const arr = [1, 2, 3, 4, 5];
+const b = arr;
+arr[0] = 10;
+
+function add(a, b) {
+  return a + b;
+}
+// console.log({ arr, b });
+
+//{username => this.setState({ username })}
+//(username) => this.setState({ username: username });
+
+var promise = new Promise(function(resolve, reject) {
+  var name = "minhaj ahmad khan ";
+  var name1 = "minhaj ahmad khan ";
+  if (name === name2) {
+    resolve();
+  }
+  esle;
+  reject();
+});
+promise
+  .then(function() {
+    console.log("you complete your promise ");
+  })
+  .catch(function() {
+    console.log("you denay your promish ");
+  });
